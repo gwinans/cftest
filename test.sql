1f05db73490f9ddfa9d1db28b30cb2a0710964cf
Geoff W. <gwinans@gmail.com>
2025-02-06 10:08:31 -0700

    .

--- test.sql
+++ test.sql
@@ -1,4 +1,4 @@
 CREATE TABLE `test` (
-    t1 int not null primary key auto_increment,
-    t3 varchar(32)
+    `t1` int not null primary key auto_increment,
+    `t3` varchar(32)
 ) engine = innodb;
